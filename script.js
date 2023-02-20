0281e07411110d794ff3df97738f8ec33df6bc34
polymorphicus <120289616+polymorphicus@users.noreply.github.com>
2023-02-20 05:13:06 -0500

    Add password match and countries validation

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
-const email = document.getElementById('mail')
+const email = document.getElementById('mail');
+const countries = document.getElementById('country');
+const pass = document.getElementById('pass');
+const match = document.getElementById('pass-confirm')
 
-//write out a function to test email validity
+
 function testEmail() {
-    //write out regexpression for correct email and put in variable
     const emailConstraint = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    //test the input value for that regular expression
-    //if it passes, the form validates
     if (emailConstraint.test(email.value) === false) {
         email.setCustomValidity('Enter a valid email with an @ after some letters');
     } else {
         email.setCustomValidity('');
     }
     email.reportValidity();
-    
-
 }
 
 
-//write a function to test the countries
-//if the stuff entered in the field matches one of the countries, now error occurs
-//if the stuff entered does not match a country, you recieve an error message
+function testCountry() {
+    const allCountries = [
+        'United States', 'France', 'Singapore', 'Japan', 'Egypt', 'Other'
+    ];
+    if (allCountries.includes(countries.value) === true) {
+        countries.setCustomValidity('');
+    } else {
+        countries.setCustomValidity('Must match one of the provided countries');
+    }
 
-//write a function to ensure the password and password confirmation match
-//if the two input values do not match, throw an error
-//if they do match, let the form submit
+    countries.reportValidity();
+}
+
+
+
+
+
+function matchPass () {
+    if(match.value === pass.value) {
+        match.setCustomValidity('');
+    } else {
+        match.setCustomValidity('Passwords do not match');
+    }
+    match.reportValidity();
+}
 
 
 //create event listeners that run each function on the corresponding input when a new input occurs.
 
-email.addEventListener('input', function() {
+email.addEventListener('input', () => {
     testEmail();
+})
+
+countries.addEventListener('input', () => {
+    testCountry();
+})
+
+match.addEventListener('input', () => {
+    matchPass();
 })
